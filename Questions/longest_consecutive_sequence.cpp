1e964853a89fe3bf47a91a8344e9fd2b03695727
PriyanshuYadav08 <priyanshu81104@gmail.com>
2026-03-19 00:31:30 +0530

    sooo

---
+++ Questions/longest_consecutive_sequence.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cout<<"enter the number of elements = ";
+    cin>>n;
+    int a[n];
+    for (int i=0; i<n; i++){
+        cout<<"enter the element = ";
+        cin>>a[i];
+    }
+
+    return 0;
+}
